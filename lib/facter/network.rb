b2056213bb33653805185ae830143672eec2afec
Brad Cowie <brad@gizmoguy.net.nz>
2016-01-20 08:57:22 +1300

    Make gateway/device name detection more robust.
     * Make regular expressions a little more flexible.
     * Support primary_network detection where gateway is a link-local
       address.

--- lib/facter/network.rb
+++ lib/facter/network.rb
@@ -14,10 +14,10 @@ Facter.add("gateway4") do
   confine :kernel => :linux
   setcode do
     gw_address = Facter::Util::Resolution.exec('/sbin/ip route show 0/0')
-    #not all network configurations will have a nexthop. 
+    #not all network configurations will have a nexthop.
     #the ip tool expresses the presence of a nexthop with the word 'via'
     if gw_address.include? ' via '
-      my_gw = gw_address.split(/\s+/)[2].to_s
+      my_gw = gw_address.match(/via\s+[^\s]+/)[0].split()[1].to_s
     end
     if is_ip?(my_gw)
       my_gw
@@ -30,10 +30,10 @@ Facter.add("gateway6") do
   confine :kernel => :linux
   setcode do
     gw_address = Facter::Util::Resolution.exec('/sbin/ip -6 route show ::/0')
-    #not all network configurations will have a nexthop. 
+    #not all network configurations will have a nexthop.
     #the ip tool expresses the presence of a nexthop with the word 'via'
     if gw_address.include? ' via '
-      my_gw = gw_address.split(/\s+/)[2].to_s
+      my_gw = gw_address.match(/via\s+[^\s]+/)[0].split()[1].to_s
     end
     if is_ip?(my_gw)
       my_gw
@@ -47,20 +47,20 @@ Facter.add("primary_network4") do
   confine :kernel => :linux
   setcode do
     gw_address = Facter::Util::Resolution.exec('/sbin/ip route show 0/0')
-    #not all network configurations will have a nexthop. 
+    #not all network configurations will have a nexthop.
     #the ip tool expresses the presence of a nexthop with the word 'via'
     if gw_address.include? ' via '
-      my_gw = gw_address.split(/\s+/)[2].to_s
+      my_gw = gw_address.match(/via\s+[^\s]+/)[0].split()[1].to_s
       if is_ip?(my_gw)
         fun = Facter::Util::Resolution.exec("/sbin/ip route get #{my_gw}").split("\n")[0]
-        fun.split(/\s+/)[2].to_s
+        fun.match(/dev\s+[^\s]+/)[0].split()[1].to_s
       end
-    #some network configurations simply have a link that all interactions are abstracted through  
+    #some network configurations simply have a link that all interactions are abstracted through
     elsif gw_address.include? 'scope link'
       #since we have no default route ip to determine where to send 'traffic not otherwise explicitly routed'
       #lets just use 8.8.8.8 as far as a route goes.
       fun = Facter::Util::Resolution.exec("/sbin/ip route get 8.8.8.8").split("\n")[0]
-      fun.split(/\s+/)[2].to_s
+      fun.match(/dev\s+[^\s]+/)[0].split()[1].to_s
     end
   end
 end
@@ -69,20 +69,24 @@ Facter.add("primary_network6") do
   confine :kernel => :linux
   setcode do
     gw_address = Facter::Util::Resolution.exec('/sbin/ip -6 route show ::/0')
-    #not all network configurations will have a nexthop. 
+    #not all network configurations will have a nexthop.
     #the ip tool expresses the presence of a nexthop with the word 'via'
     if gw_address.include? ' via '
-      my_gw = gw_address.split(/\s+/)[2].to_s
+      my_gw = gw_address.match(/via\s+[^\s]+/)[0].split()[1].to_s
       if is_ip?(my_gw)
-        fun = Facter::Util::Resolution.exec("/sbin/ip -6 route get #{my_gw}").split("\n")[0]
-        fun.split(/\s+/)[6].to_s
+        if my_gw.start_with?('fe80:')
+          gw_address.match(/dev\s+[^\s]+/)[0].split()[1].to_s
+        else
+          fun = Facter::Util::Resolution.exec("/sbin/ip -6 route get #{my_gw}")
+          fun.match(/dev\s+[^\s]+/)[0].split()[1].to_s
+        end
       end
-    #some network configurations simply have a link that all interactions are abstracted through  
+    #some network configurations simply have a link that all interactions are abstracted through
     elsif gw_address.include? 'scope link'
       #since we have no default route ip to determine where to send 'traffic not otherwise explicitly routed'
       #lets just use 8.8.8.8 as far as a route goes.
-      fun = Facter::Util::Resolution.exec("/sbin/ip -6 route get 2001:4860:4860::8888").split("\n")[0]
-      fun.split(/\s+/)[6].to_s
+      fun = Facter::Util::Resolution.exec("/sbin/ip -6 route get 2001:4860:4860::8888")
+      fun.match(/dev\s+[^\s]+/)[0].split()[1].to_s
     end
   end
 end
